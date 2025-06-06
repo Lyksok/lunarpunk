424526863d6cf5b8328b9fb57f2e5990c53a7d91
Lykso <jans.guillope@proton.me>
2025-06-06 02:02:09 +0200

    feat: resized window, game title

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::WindowResolution;
 use bevy::winit::WinitSettings;
 use lunarpunk::{
     camera::menu_camera::spawn_camera, menu::button_interactions::quit_button,
@@ -7,7 +8,14 @@ use lunarpunk::{
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Lunarpunk".to_string(),
+                resolution: WindowResolution::new(1280.0, 720.0).with_scale_factor_override(1.0),
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, (setup, spawn_camera))
         .add_systems(Update, quit_button)
